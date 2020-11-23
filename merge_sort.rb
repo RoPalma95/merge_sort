c95576a510629a103b8f219e87463ab054365337
RoPalma95 <r.palma995@gmail.com>
2020-11-23 16:09:45 -0600

    Added comments and styling fixes

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,21 +1,19 @@
+# frozen_string_literal: true
+
 def merge_sort(arr)
   return arr if arr.length < 2
 
   merged = []
 
   if arr.length >= 2
-    lh = arr.slice(0, arr.length/2)
-    rh = arr.slice(arr.length/2, arr.length)
+    lh = arr.slice(0, arr.length / 2)
+    rh = arr.slice(arr.length / 2, arr.length)
     sorted_lh = merge_sort(lh)
     sorted_rh = merge_sort(rh)
-  
+
     until sorted_lh.empty?
       index = 0
-      if sorted_rh.empty? || sorted_lh[index] < sorted_rh[index]
-        merged << sorted_lh.shift
-      else
-        merged << sorted_rh.shift
-      end
+      merged << (sorted_rh.empty? || sorted_lh[index] < sorted_rh[index] ? sorted_lh.shift : sorted_rh.shift)
       index += 1
     end
     merged << sorted_rh unless sorted_rh.empty?
@@ -23,5 +21,13 @@ def merge_sort(arr)
   merged.flatten
 end
 
-p merge_sort([31,53,99,6,89,28,30,73,5,25,3,78,96,79,76])
-p [31,53,99,6,89,28,30,73,5,25,3,78,96,79,76].sort
+# example 50-item array to test the #merge_sort method
+
+example = []
+50.times do
+  example.push(rand(100))
+end
+
+# check if #merge_sort method produces same results as Array#sort
+
+p merge_sort(example) == example.sort
